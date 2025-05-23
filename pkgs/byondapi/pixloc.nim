c49e73c6d6d4a38240cab34f87f1a5ae0f94cc0c
Antoonij <pisclov123pis2@gmail.com>
2025-05-23 09:35:42 +0200

    simplify pixloc

--- pkgs/byondapi/pixloc.nim
+++ pkgs/byondapi/pixloc.nim
@@ -1,16 +1,13 @@
 import ../byondapi_raw/byondapi, value/value, error
 
-when BYOND_MAJOR < 516:
-  static:
-    error "This file is only compatible with BYOND version 516+"
+when BYOND_MAJOR >= 516:
+  type
+      ByondPixLoc* = CByondPixLoc
 
-type
-    ByondPixLoc* = CByondPixLoc
+  proc getPixLoc*(src {.byref.}: ByondValue): ByondPixLoc =
+    result = ByondPixLoc()
+    handleByondError(Byond_GetPixLoc(addr src, addr result))
 
-proc getPixLoc*(src {.byref.}: ByondValue): ByondPixLoc =
-  result = ByondPixLoc()
-  handleByondError(Byond_GetPixLoc(addr src, addr result))
-
-proc getBoundPixLoc*(src {.byref.}: ByondValue, dir: u1c): ByondPixLoc =
-  result = ByondPixLoc()
-  handleByondError(Byond_BoundPixLoc(addr src, dir, addr result))
+  proc getBoundPixLoc*(src {.byref.}: ByondValue, dir: u1c): ByondPixLoc =
+    result = ByondPixLoc()
+    handleByondError(Byond_BoundPixLoc(addr src, dir, addr result))
